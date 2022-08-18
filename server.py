b590c5818e2a8a0304dbe35edd891339349e1279
张俊杰 <57305595@qq.com>
2022-08-18 15:28:30 +0800

    修改为GPU

--- server.py
+++ server.py
@@ -3,43 +3,37 @@ import grpc
 import pd.MBARTLarge50_pb2  as mbPB
 import pd.MBARTLarge50_pb2_grpc as mbRPC
 import logging
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+import torch
+
+import dl_translate as dlt
 
 class Mbart(mbRPC.MBARTLarge50Servicer):
 
     def InterpretE2C(self, request, context):
         m = interpret(request.message)
-        return mbPB.ChineseReply(message=str(m[0]))
+        return mbPB.ChineseReply(message=str(m))
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=500))
     mbRPC.add_MBARTLarge50Servicer_to_server(Mbart(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    print("启动成功 [::]:50051 ")
+    print("启动成功 [::]:50051")
     server.wait_for_termination()
 
 
 # 翻译
 def interpret(article):
-    model_inputs = tokenizer(article,return_tensors="pt")
-
-    generated_tokens = model.generate(
-        **model_inputs,
-        max_length=500,
-        forced_bos_token_id=tokenizer.lang_code_to_id["zh_CN"]
-    )
-
-    return tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-
+    v =  mt.translate(article, source=dlt.lang.ENGLISH, target=dlt.lang.CHINESE)
+    return v
 
 
 if __name__ == '__main__':
-
+    device = torch.device("cuda")
     print("加载训练结果")
-    model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-one-to-many-mmt")
-    tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-one-to-many-mmt", src_lang="en_XX")
+    mt = dlt.TranslationModel(device="gpu")
+    mt = dlt.TranslationModel("facebook/mbart-large-50-many-to-many-mmt")
     print("启动监听")
     logging.basicConfig()
     serve()
